be227210d199a20e506a4b4d61f348b291b5ed91
Noah Meyerhans <nmeyerhans@linux.microsoft.com>
2026-09-17 14:14:39 -0400

    Add GPE entries for Surface Laptop 5 for Business
    Signed-off-by: Noah Meyerhans <nmeyerhans@linux.microsoft.com>

--- drivers/platform/surface/surface_gpe.c
+++ drivers/platform/surface/surface_gpe.c
@@ -57,6 +57,14 @@ static const struct property_entry lid_device_props_l57[] = {
  */
 static const struct dmi_system_id dmi_lid_device_table[] = {
 	{
+		.ident = "Surface Laptop 5",
+		.matches = {
+			DMI_EXACT_MATCH(DMI_SYS_VENDOR, "Microsoft Corporation"),
+			DMI_EXACT_MATCH(DMI_PRODUCT_SKU, "Surface_Laptop_5_for_Business_1979"),
+		},
+		.driver_data = (void *)lid_device_props_l52,
+	},
+	{
 		.ident = "Surface Pro 4",
 		.matches = {
 			DMI_EXACT_MATCH(DMI_SYS_VENDOR, "Microsoft Corporation"),
